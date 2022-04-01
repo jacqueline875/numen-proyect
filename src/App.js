26d06b0dd642e185b7f8b9234d8e45de47d2b73c
jacquelineim <jacqueline_875@hotmail.com>
2022-04-01 19:51:46 -0300

    mi primer commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import Cards from "./components/Cards";
 import Footer from "./components/Footer";
-
+//mi primer comentario
 const App = () => {
   return (
     <div>
