60f61d36c3cb55d8648e0cd1335a72e0e3b134c3
OsvaldoColmenares <ovacolmenares@gmail.com>
2022-04-15 00:08:41 -0300

    Se agrega componente Cards y SweetAlert2

--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ import Redes from "./components/Redes";
 import Carrito from "./components/Carrito";
 import Carousel from "./components/Carousel";
 import Banner from "./components/Banner";
+import { cardsList } from "./data"; 
+import Swal from 'sweetalert2'
 
 const App = () => {
   return (
     <div>
       <NavBar/>
+      <Carrito/>  
       <Banner/>
-      <Cards/>
-      <Carousel/>           
-      <Footer/>
-      <Redes/>
-      <Carrito/>
+      <Cards list={cardsList}/>       
+      <Carousel/> 
+      <Redes/>        
+      <Footer/> 
     </div>
   );
 };
 
 export default App;
-//prueba
---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const cardsList = [
+    {
+        "id": "1",
+        "imageUrl": "https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png",
+        "category": "sfsdf",
+        "description": "dfdfdsfd1",
+        "price": "$100"
+    },
+    {
+        "id": "2",
+        "imageUrl": "https://user-images.githubusercontent.com/2805249/64069998-305de300-cc9a-11e9-8ae7-5a0fe00299f2.png",
+        "category": "dfdfgfh",
+        "description": "fghfghgfhgfh",
+        "price": "$150"
+    },
+    {
+        "id": "3",
+        "imageUrl": "https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png",
+        "category": "sdfdsfdsf",
+        "description": "sdfdfsf",
+        "price": "$803"
+    },
+    {
+        "id": "4",
+        "imageUrl": "https://img1.freepng.es/20190204/aiz/kisspng-sneakers-shoe-nike-air-max-bw-se-wmns-4-size-44-5c582d07710202.1376874515492825674629.jpg",
+        "category": "dfghfdgf",
+        "description": "dhfgfd",
+        "price": "$500"
+    },
+    {
+        "id": "5",
+        "imageUrl": "https://w7.pngwing.com/pngs/837/990/png-transparent-oklahoma-city-thunder-nike-nba-all-star-game-sneakers-shoe-nike-white-outdoor-shoe-playstation-4.png",
+        "category": "sfsdf",
+        "description": "dfdfdsfd1",
+        "price": "$100"
+    },
+    {
+        "id": "6",
+        "imageUrl": "https://imagenes.elpais.com/resizer/xIHbQ3bXLB81-9NjJtH7SaoHNqI=/414x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/TAL6UUKL7BHCRCAAZL262ATKPI.png",
+        "category": "dfdfgfh",
+        "description": "fghfghgfhgfh",
+        "price": "$150"
+    },
+    {
+        "id": "7",
+        "imageUrl": "https://media.vogue.es/photos/6120c767358123b52e2edbbe/master/w_320%2Cc_limit/Captura%2520de%2520pantalla%25202021-08-21%2520a%2520las%252011.29.06.png",
+        "category": "dfdfgfh",
+        "description": "fghfghgfhgfh",
+        "price": "$150"
+    }
+   
+]
