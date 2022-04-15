94e1b49c55b3d3daa0bcf2afd7b2d3111bf610ca
OsvaldoColmenares <ovacolmenares@gmail.com>
2022-04-15 01:02:45 -0300

    Se modifica Cards

--- src/data.js
+++ src/data.js
@@ -1,52 +1,31 @@
 export const cardsList = [
     {
         "id": "1",
-        "imageUrl": "https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png",
+        "imageUrl": "https://cdn.shopify.com/s/files/1/0490/7191/5158/products/Shake-1_854001c4-61f8-4ebb-a318-2e6af38b721f_400x.png?v=1645549218",
         "category": "sfsdf",
         "description": "dfdfdsfd1",
         "price": "$100"
     },
     {
         "id": "2",
-        "imageUrl": "https://user-images.githubusercontent.com/2805249/64069998-305de300-cc9a-11e9-8ae7-5a0fe00299f2.png",
+        "imageUrl": "https://cdn.shopify.com/s/files/1/0490/7191/5158/products/Alnilam4.png?v=1640225660",
         "category": "dfdfgfh",
         "description": "fghfghgfhgfh",
         "price": "$150"
     },
     {
         "id": "3",
-        "imageUrl": "https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png",
+        "imageUrl": "https://cdn.shopify.com/s/files/1/0490/7191/5158/products/Hero-1_bd21e761-850b-48df-a859-e707ef9a985e.png?v=1645194512",
         "category": "sdfdsfdsf",
         "description": "sdfdfsf",
         "price": "$803"
     },
     {
         "id": "4",
-        "imageUrl": "https://img1.freepng.es/20190204/aiz/kisspng-sneakers-shoe-nike-air-max-bw-se-wmns-4-size-44-5c582d07710202.1376874515492825674629.jpg",
+        "imageUrl": "https://cdn.shopify.com/s/files/1/0490/7191/5158/products/Crux-1.png?v=1636045676",
         "category": "dfghfdgf",
         "description": "dhfgfd",
         "price": "$500"
-    },
-    {
-        "id": "5",
-        "imageUrl": "https://w7.pngwing.com/pngs/837/990/png-transparent-oklahoma-city-thunder-nike-nba-all-star-game-sneakers-shoe-nike-white-outdoor-shoe-playstation-4.png",
-        "category": "sfsdf",
-        "description": "dfdfdsfd1",
-        "price": "$100"
-    },
-    {
-        "id": "6",
-        "imageUrl": "https://imagenes.elpais.com/resizer/xIHbQ3bXLB81-9NjJtH7SaoHNqI=/414x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/TAL6UUKL7BHCRCAAZL262ATKPI.png",
-        "category": "dfdfgfh",
-        "description": "fghfghgfhgfh",
-        "price": "$150"
-    },
-    {
-        "id": "7",
-        "imageUrl": "https://media.vogue.es/photos/6120c767358123b52e2edbbe/master/w_320%2Cc_limit/Captura%2520de%2520pantalla%25202021-08-21%2520a%2520las%252011.29.06.png",
-        "category": "dfdfgfh",
-        "description": "fghfghgfhgfh",
-        "price": "$150"
-    }
+    }  
    
 ]
