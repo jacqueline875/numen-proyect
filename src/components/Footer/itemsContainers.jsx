fa5ef0bda483dc5bd60c9efc240241cf6115e483
jacquelineim <jacqueline_875@hotmail.com>
2022-04-22 23:50:42 -0300

    itemscontainers

--- src/components/Footer/itemsContainers.jsx
+++ src/components/Footer/itemsContainers.jsx
@@ -1,8 +1,8 @@
-import Item from "./Item";
+import Item from "./items";
 import { PRODUCTS, RESOURCES, COMPANY, SUPPORT } from "./Menus";
 const ItemsContainer = () => {
 return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16">
+    <div className="grid grid-cols-1 gap-6 px-5 py-16 sm:grid-cols-3 lg:grid-cols-4 sm:px-8">
     <Item Links={PRODUCTS} title="PRODUCTS" />
     <Item Links={RESOURCES} title="RESOURCES" />
     <Item Links={COMPANY} title="COMPANY" />
