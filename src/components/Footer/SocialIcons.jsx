410c2353b27732b79d3fd5ba6be339a4cf4548b2
jacquelineim <jacqueline_875@hotmail.com>
2022-04-23 01:56:27 -0300

    SocialIcons

--- src/components/Footer/SocialIcons.jsx
+++ src/components/Footer/SocialIcons.jsx
@@ -7,7 +7,7 @@ return (
         <span key={icon.name} className="p-2 cursor-pointer inline-flex items-center
         rounded-full bg-gray-700 mx-1.5 text-xl hover:text-gray-100 hover:bg-teal-500
         duration-300 ">
-        <ion-icons name={icon.name}></ion-icons>
+        <ion-icon name={icon.name}></ion-icon>
         </span>
     ))}
     </div>
