be63b8a4f0793661ca6ced005c044efcd951ea48
FacundoRoman <faculuca.roman@gmail.com>
2022-05-06 23:58:40 -0300

    agrega imagenes a carousel

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -42,31 +42,22 @@ const Carousel = () => {
         <h2> Multiple items </h2>
         <Slider {...settings}>
           <div>
-            <img src="https://cdn.shopify.com/s/files/1/0490/7191/5158/products/Alnilam4.png?v=1640225660" alt="" />
+            <img src="https://cdn.shopify.com/s/files/1/0490/7191/5158/products/Shake-1_854001c4-61f8-4ebb-a318-2e6af38b721f_400x.png?v=1645549218" alt="" />
+          </div>
+          <div>
+          <img src="https://i.ibb.co/rGfP7mp/Rectangle-59-1.png" alt="" />
+          </div>
+          <div>
+          <img src="https://cdn.shopify.com/s/files/1/0490/7191/5158/products/Crux-1.png?v=1636045676" alt="" />
           </div>
           <div>
           <img src="https://cdn.shopify.com/s/files/1/0490/7191/5158/products/Alnilam4.png?v=1640225660" alt="" />
           </div>
           <div>
-          <img src="https://cdn.shopify.com/s/files/1/0490/7191/5158/products/Alnilam4.png?v=1640225660" alt="" />
+          <img src="https://i.ibb.co/J2BtZdg/Rectangle-56-1.png" alt="" />
           </div>
           <div>
-          <img src="https://cdn.shopify.com/s/files/1/0490/7191/5158/products/Alnilam4.png?v=1640225660" alt="" />
-          </div>
-          <div>
-          <img src="https://cdn.shopify.com/s/files/1/0490/7191/5158/products/Alnilam4.png?v=1640225660" alt="" />
-          </div>
-          <div>
-          <img src="https://cdn.shopify.com/s/files/1/0490/7191/5158/products/Alnilam4.png?v=1640225660" alt="" />
-          </div>
-          <div>
-            <h3>7</h3>
-          </div>
-          <div>
-            <h3>8</h3>
-          </div>
-          <div>
-            <h3>9</h3>
+          <img src="https://cdn.shopify.com/s/files/1/0490/7191/5158/products/Crux-1.png?v=1636045676" alt="" />
           </div>
         </Slider>
       </div>
