151c1a79b17158c43fa1907ea8f40e8d19a1f200
OsvaldoColmenares <ovacolmenares@gmail.com>
2022-05-06 23:36:02 -0300

    se agrega intervalo a las imagenes del banner

--- src/components/BannerPrincipal/index.jsx
+++ src/components/BannerPrincipal/index.jsx
@@ -5,10 +5,10 @@ const BannerPrincipal = () => {
     <div className="bg-cover bg-center h-auto text-white py-64 px-10 object-fill bg-[url('https://mc.net.co/wp-content/uploads/2020/12/xcomputo-tecnologia-panorama.jpg.pagespeed.ic.YJVB-FRLUJ.webp')]" >
         
        <div className='md:w-1/2'>
-        <p className='font-bold text-sm uppercase'>Ssdfdsfsdfdsf</p>
-        <p className='text-3xl font-bold'>Mdgffdgdfg dfgdfgdfg</p>
-        <p className='text-2xl mb-10 leading-none'>Adgdfgf dfgdfg dfgdgd dfg dfg</p>
-        <a href='#' className='bg-purple py-4 px-8 text-white font-bold uppercase text-xs rounded hover:bg-gray-200 hover:text-gray-800'>Cdfgdfg</a>
+        <p className='font-bold text-sm uppercase'>Mira las mejores ofertas</p>
+        <p className='text-3xl font-bold'>Todo el mes de mayo</p>
+        <p className='text-2xl mb-10 leading-none'>Notebook HP</p>
+        <a href='#' className='bg-purple py-4 px-8 text-white font-bold uppercase text-xs rounded hover:bg-gray-200 hover:text-gray-800'>Ver más</a>
         </div>  
     </div>
   )
--- src/data.js
+++ src/data.js
@@ -2,42 +2,42 @@ export const products = [
     {
         "id": "1",
         "imageUrl": "https://cdn.shopify.com/s/files/1/0490/7191/5158/products/Shake-1_854001c4-61f8-4ebb-a318-2e6af38b721f_400x.png?v=1645549218",
-        "category": "sfsdf",
+        "category": "Categoria",
         "name": "Auriculares",
         "price": "100"
     },
     {
         "id": "2",
         "imageUrl": "https://cdn.shopify.com/s/files/1/0490/7191/5158/products/Alnilam4.png?v=1640225660",
-        "category": "dfdfgfh",
+        "category": "Categoria",
         "name": "Teclado",
         "price": "150"
     },
     {
         "id": "3",
         "imageUrl": "https://i.ibb.co/rGfP7mp/Rectangle-59-1.png",
-        "category": "sdfdsfdsf",
+        "category": "Categoria",
         "name": "Joystick",
         "price": "803"
     },
     {
         "id": "4",
         "imageUrl": "https://cdn.shopify.com/s/files/1/0490/7191/5158/products/Crux-1.png?v=1636045676",
-        "category": "dfghfdgf",
+        "category": "Categoria",
         "name": "Teclado Gamer",
         "price": "500"
     },  
     {
         "id": "5",
         "imageUrl": "https://i.ibb.co/J2BtZdg/Rectangle-56-1.png",
-        "category": "dfghfdgf",
+        "category": "Categoria",
         "name": "Parlantes",
         "price": "500"
     },
     {
         "id": "6",
         "imageUrl": "https://cdn.shopify.com/s/files/1/0490/7191/5158/products/Crux-1.png?v=1636045676",
-        "category": "dfghfdgf",
+        "category": "Categoria",
         "name": "Teclado Razer",
         "price": "500"
     }
