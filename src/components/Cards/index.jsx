9b49cbc1287297d78ab4510be57f0ffb6a4903e7
OsvaldoColmenares <ovacolmenares@gmail.com>
2022-05-13 21:06:15 -0300

    arreglo unos detalles

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -6,7 +6,8 @@ const Cards = (props) => {
   const { products, onAdd } = props;
 
   return (
-    <div id="Productos">     
+    <div id="Productos">   
+      <p className="text-center mt-10 text-3xl font-bold text-silver">Destacados</p>  
       <div className="flex flex-row flex-wrap items-center justify-center">
         {products.map((product) => (
           <Product key={product.id} product={product} onAdd={onAdd} />
